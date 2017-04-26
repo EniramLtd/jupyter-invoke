6ec843f9afae88cd3a62c2427fe715be2e661c9a
Jarno Saarimäki <jarno.saarimaki@eniram.fi>
2017-04-26 15:58:18 +0300

    Parse the environment variable (#2)

--- jupyter_invoke/__init__.py
+++ jupyter_invoke/__init__.py
@@ -1,3 +1,4 @@
+import os
 from notebook.utils import url_path_join
 from .invoke import InvokeNotebookHandler
 from .response import respond, JSON_OUTPUT, CSV_OUTPUT
@@ -9,6 +10,22 @@ def _jupyter_server_extension_paths():
     }]
 
 
+def get_extension_parameters():
+    """Additional parameters for the extension with environment variables
+    
+    Parameters:
+        
+        - INVOKE_FILENAME_PATTERN: regex for matching the end of notebook
+          name (before ``.ipynb``) to allow invocation.  If not specified,
+          all notebooks are allowed to be invoked.
+    
+    """
+    nb_name_ptrn = os.environ.get('INVOKE_FILENAME_PATTERN', '.+\.ipynb')
+    if not nb_name_ptrn.endswith('\.ipynb'):
+        nb_name_ptrn += '\.ipynb'
+    return {'nb_name_ptrn': nb_name_ptrn}
+
+
 def load_jupyter_server_extension(nb_server_app):
     """Called when the extension is loaded
 
@@ -16,9 +33,11 @@ def load_jupyter_server_extension(nb_server_app):
     http://jupyter-notebook.readthedocs.io/en/latest/extending/handlers.html
 
     """
+    params = get_extension_parameters()
     web_app = nb_server_app.web_app
     host_pattern = '.*$'
     route_pattern = url_path_join(web_app.settings['base_url'],
-                                  '/invoke/(.+)$')
+                                  '/invoke/(.+{})$'
+                                  ''.format(params['nb_name_ptrn']))
     web_app.add_handlers(host_pattern, [(route_pattern,
                                          InvokeNotebookHandler)])
